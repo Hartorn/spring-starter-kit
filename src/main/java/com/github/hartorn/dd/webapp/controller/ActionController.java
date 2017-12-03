ef16ae55d30ceea8fd46fe7c871146e503cde538
Hartorn <bazirehoussin@gmail.com>
2017-12-03 22:26:34 +0100

    Using Lombok

--- src/main/java/com/github/hartorn/dd/webapp/controller/ActionController.java
+++ src/main/java/com/github/hartorn/dd/webapp/controller/ActionController.java
@@ -11,18 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.hartorn.dd.webapp.model.ActionService;
 import com.github.hartorn.dd.webapp.model.generated.Actions;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 /**
  * @author Hartorn
  *
  */
 @RestController
+@Log4j2
+@RequiredArgsConstructor
 public class ActionController {
 
-	@Autowired
-	private ActionService actionService;
+    private final ActionService actionService;
 
 	@RequestMapping(method = RequestMethod.GET, path = "/actions")
 	public Iterable<Actions> greeting() {
+        log.info("Calling findAll");
 		return this.actionService.findAll();
 	}
 
